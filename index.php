16aded9b03541ecea4c4cea60ae6e7fd425f118e
Gatis Ieviņš <personal@maskless.id.lv>
2023-07-20 15:43:52 +0300

    Creating the visual frontend

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// Autoload Composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Global constants
+const DIR_PROJECT = __DIR__;
+
+// Showing off the contents
+include_once(DIR_PROJECT . "/jobOffersBulletin.php");
+//*/
